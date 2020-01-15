1b203229a4e7fed358ba4602ab2efa245264b9b1
jadencodes <jaden@automox.com>
2020-01-15 12:51:42 -0700

    Changed supervisor to be dynamic with add_pool

--- lib/pool_supervisor.ex
+++ lib/pool_supervisor.ex
@@ -1,31 +1,31 @@
 defmodule ExRabbitPool.PoolSupervisor do
-  use Supervisor
+  use DynamicSupervisor
 
   @type config :: [rabbitmq_config: keyword(), connection_pools: list()]
 
   @spec start_link(config()) :: Supervisor.on_start()
   def start_link(config) do
-    Supervisor.start_link(__MODULE__, config)
+    IO.inspect config
+    DynamicSupervisor.start_link(__MODULE__, config, name: ExRabbitPool.DynamicSupervisor)
   end
 
   @spec start_link(config(), atom()) :: Supervisor.on_start()
   def start_link(config, name) do
-    Supervisor.start_link(__MODULE__, config, name: name)
+    IO.puts "Starting with name #{name}"
+    DynamicSupervisor.start_link(__MODULE__, config, name: name)
   end
 
   @impl true
-  def init(config) do
-    children =
-      for pool_config <- Keyword.get(config, :connection_pools, []) do
-        rabbitmq_config = Keyword.get(config, :rabbitmq_config, [])
-        {_, pool_id} = Keyword.fetch!(pool_config, :name)
-        # We are using poolboy's pool as a fifo queue so we can distribute the
-        # load between workers
-        pool_config = Keyword.merge(pool_config, strategy: :fifo)
-        :poolboy.child_spec(pool_id, pool_config, rabbitmq_config)
-      end
+  def init(_config) do
+    DynamicSupervisor.init(strategy: :one_for_one, name: ExRabbitPool.DynamicSupervisor)
+  end
 
-    opts = [strategy: :one_for_one]
-    Supervisor.init(children, opts)
+  def add_pool(rabbitmq_config, pool_config) do
+    {_, pool_id} = Keyword.fetch!(pool_config, :name)
+    # We are using poolboy's pool as a fifo queue so we can distribute the
+    # load between workers
+    pool_config = Keyword.merge(pool_config, strategy: :fifo)
+    spec = :poolboy.child_spec(pool_id, pool_config, rabbitmq_config)
+    DynamicSupervisor.start_child(ExRabbitPool.DynamicSupervisor, spec)
   end
 end
